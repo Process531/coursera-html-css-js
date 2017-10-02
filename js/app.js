f536667a0d0cbc2dc7dbbb8356b941dae883986f
sergei.vitkovskiy <sergei.vitkovskiy@dct.ua>
2017-10-02 14:39:43 +0300

    Functions

--- js/app.js
+++ js/app.js
@@ -8,13 +8,48 @@
 // console.log('Company CEO name is: ' + company.ceo.firstName);
 // console.log(company['name']);
 
-let facebook = {
-    name: 'Facebook',
-    ceo: {
-        firstName: 'Makrk',
-        favColor: 'blue'
-    },
-    'stock of company': 110
-};
+// let facebook = {
+//     name: 'Facebook',
+//     ceo: {
+//         firstName: 'Makrk',
+//         favColor: 'blue'
+//     },
+//     'stock of company': 110
+// };
+//
+// console.log(facebook);
 
-console.log(facebook);
+function multiply(x, y) {
+    return x * y;
+}
+
+multiply.version = 'v.1.0.0';
+console.log(multiply);
+
+// Function factory
+
+function makeMultiplier(multiplier) {
+    let myFunc = function (x) {
+        return multiplier * x;
+    };
+    return myFunc;
+}
+
+
+// console.log(makeMultiplier(3));
+// let multiplyBy3 = makeMultiplier(3);
+// console.log(multiplyBy3(10));
+// let doubleAll = makeMultiplier(2);
+// console.log(doubleAll(100));
+
+// Passing functions as arguments
+function doOperationOn(x, operation) {
+    return operation(x);
+}
+// let result = doOperationOn(5, multiplyBy3);
+// console.log(result);
+// result = doOperationOn(100, doubleAll);
+// console.log(result);
+
+
+
