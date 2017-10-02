f6ae85c9a721b6b0457453a9a1b458638bb4eed6
sergei.vitkovskiy <sergei.vitkovskiy@dct.ua>
2017-10-02 13:51:38 +0300

    Objects

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// let company = new Object();
+// company.name = 'Facebook';
+// company.ceo = new Object();
+// company.ceo.firstName = 'Mark';
+// company.ceo.favColor = 'blue';
+// console.log(company);
+//
+// console.log('Company CEO name is: ' + company.ceo.firstName);
+// console.log(company['name']);
+
+let facebook = {
+    name: 'Facebook',
+    ceo: {
+        firstName: 'Makrk',
+        favColor: 'blue'
+    },
+    'stock of company': 110
+};
+
+console.log(facebook);
