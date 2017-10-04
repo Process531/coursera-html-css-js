b69e64dbc410dff7be852747169b4a598fe19431
sergei.vitkovskiy <sergei.vitkovskiy@dct.ua>
2017-10-04 21:24:55 +0300

    Handling Events

--- js/lecture-dom.js
+++ js/lecture-dom.js
@@ -2,22 +2,38 @@
 // console.log(window.document);
 // console.log(document instanceof HTMLDocument);
 // console.dir(HTMLDocument);
+document.addEventListener('DOMContentLoaded',
+    function (event) {
 
-function sayHello() {
 
-    let name = document.getElementById('name').value;
-    let message = `<h2>Hello ${name}</h2>`;
-    if (name) {
-        this.textContent = 'Said it!';
-        document.getElementById('content').innerHTML = message;
+        function sayHello(event) {
+            let name = document.getElementById('name').value;
+            let message = `<h2>Hello ${name}</h2>`;
+            if (name) {
+                this.textContent = 'Said it!';
+                document.getElementById('content').innerHTML = message;
+            }
+
+            if (name === 'student') {
+
+                let title = document.querySelector('h1').textContent;
+                title += '  & Lovin it!';
+                document.querySelector('h1').textContent = title;
+            }
+        }
+
+        document.querySelector('button').addEventListener('click', sayHello);
+
+        document.querySelector('body').addEventListener('mousemove', (event) => {
+            if (event.shiftKey) {
+                console.log('x: ' + event.clientX);
+                console.log('y: ' + event.clientY);
+            }
+        });
+
+
     }
+);
 
-    if(name === 'student') {
 
-        let title = document.querySelector('h1').textContent;
-        title += '  & Lovin it!';
-        document.querySelector('h1').textContent = title;
-    }
-}
 
-document.querySelector('button').addEventListener('click', sayHello);
