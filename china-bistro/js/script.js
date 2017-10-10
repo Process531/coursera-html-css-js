f5fa8bdf615593fc79baf28e2cef44714da741e9
sergei.vitkovskiy <sergei.vitkovskiy@dct.ua>
2017-10-10 16:09:05 +0300

    Lecture 63.

--- china-bistro/js/script.js
+++ china-bistro/js/script.js
@@ -55,6 +55,21 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         return string;
     }
 
+// Remove the class 'active' from home and switch to Menu button
+    var switchMenuToActive = function () {
+        // Remove 'active' from home button
+        var classes = document.querySelector("#navHomeButton").className;
+        classes = classes.replace(new RegExp("active", "g"), "");
+        document.querySelector("#navHomeButton").className = classes;
+
+        // Add 'active' to menu button if not already there
+        classes = document.querySelector("#navMenuButton").className;
+        if (classes.indexOf("active") == -1) {
+            classes += " active";
+            document.querySelector("#navMenuButton").className = classes;
+        }
+    };
+
 // On page load (before images or CSS)
     document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -99,6 +114,9 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 $ajaxUtils.sendGetRequest(
                     categoryHtml,
                     function (categoryHtml) {
+                        // Switch CSS class active to menu button
+                        switchMenuToActive();
+
                         var categoriesViewHtml =
                             buildCategoriesViewHtml(categories,
                                 categoriesTitleHtml,
@@ -152,6 +170,9 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 $ajaxUtils.sendGetRequest(
                     menuItemHtml,
                     function (menuItemHtml) {
+                        // Switch CSS class active to menu button
+                        switchMenuToActive();
+
                         var menuItemsViewHtml =
                             buildMenuItemsViewHtml(categoryMenuItems,
                                 menuItemsTitleHtml,
